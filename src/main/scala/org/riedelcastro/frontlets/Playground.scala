5687eeafbdd64c68e97675784318a74a091cfc7f
riedelcastro <sebastian.riedel@gmail.com>
2012-08-14 16:48:42 +0100

    started to play around with frontlet compilation

--- src/main/scala/org/riedelcastro/frontlets/Playground.scala
+++ src/main/scala/org/riedelcastro/frontlets/Playground.scala
@@ -7,41 +7,46 @@ package org.riedelcastro.frontlets
 object Playground {
 
   def main(args: Array[String]) {
-    trait Test[T <: Test[T]] {
-      def create(): T
-    }
-    class A extends Test[A] {
-      def create() = new A
-    }
-    class B extends Test[B] {
-      def create() = getClass.getConstructor().newInstance().asInstanceOf[B]
-    }
-    class Outer {
-      val inner = new Inner
-      def create() = {
-        println(getClass.isMemberClass)
-//        println(getClass.isLocalClass)
+    trait Term[+T]
+    case class Binding[+T](variable: Var[T], value: T)
+    case class Var[+T](name: String) extends Term[T]
+    case class Assignment[+T](variable: Var[T], term: Term[T])
+    case class Program(assignments: Seq[Assignment[_]])
+    case class Const[+T](value: T) extends Term[T]
+    case class SlotValue[F <: AbstractFrontlet, T](frontlet: Term[F], slot: F => F#Slot[T]) extends Term[T]
 
+    trait Compilation {
+      def execute(bindings: Seq[Binding[_]]): Seq[Binding[_]]
+    }
+
+    case class ArrayFrontlet(ints: Array[Int],
+                             strings: Array[String],
+                             frontlets: Array[ArrayFrontlet])
+
+    def compile(program: Program): Compilation = {
+
+      def evaluate[T](term: Term[T], bindings: Seq[Binding[_]]): T = term match {
+        case SlotValue(f, s) => s(evaluate(f, bindings)).value
+        case Const(v) => v
+        case _ => sys.error("Can't evaluate " + term)
       }
-      class Inner {
-        def create() = {
-          println(getClass.isMemberClass)
-//          println(getClass.isAnonymousClass)
-
-          val f = getClass().getDeclaredField("$outer")
-          val o = f.get(this)
-          val c = getClass.getConstructor(o.getClass)
-          val n = c.newInstance(o)
-          println(n)
-          println(f)
-          println(o)
-          n
+      new Compilation {
+        def execute(bindings: Seq[Binding[_]]) = {
+          for (assignment <- program.assignments) yield {
+            Binding(assignment.variable, evaluate(assignment.term, bindings))
+          }
         }
       }
     }
-    val outer = new Outer
-    outer.inner.create()
-    outer.create()
+
+    class Person extends Frontlet {
+      val age = IntSlot("age")
+    }
+
+
+    var result = Var[Int]("result")
+    val compilation = compile(Program(Seq(Assignment(result, SlotValue[Person, Int](Const(new Person().age(36)), _.age)))))
+    println(compilation.execute(Seq.empty))
 
   }
 
