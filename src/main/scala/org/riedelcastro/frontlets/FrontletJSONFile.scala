12c33452e24bb61d8c7e5eaba3af6917591b6ba9
riedelcastro <sebastian.riedel@gmail.com>
2012-09-11 11:52:50 +0100

    factored out some generic Frontlet IO code

--- src/main/scala/org/riedelcastro/frontlets/FrontletJSONFile.scala
+++ src/main/scala/org/riedelcastro/frontlets/FrontletJSONFile.scala
@@ -1,7 +1,6 @@
 package org.riedelcastro.frontlets
 
-import java.io.{FileInputStream, PrintStream, File}
-import collection.JavaConversions._
+import java.io.{FileInputStream, InputStream, PrintStream, File}
 import collection.mutable
 import org.riedelcastro.nurupo.Util
 
@@ -14,12 +13,7 @@ class FrontletJSONFile[F<:AbstractFrontlet](filename:String, constructor: () => 
   private lazy val file = new File(filename)
   private lazy val out = new PrintStream(file)
 
-  def iterator = {
-    val parser = FrontletJacksonMapper.mapper.getJsonFactory.createJsonParser(file)
-    val maps = Util.untilException(FrontletJacksonMapper.readValue[mutable.Map[String, Any]](parser), t => true)
-    maps.map(constructor().setMap(_))
-//    FrontletJacksonMapper.readValues[mutable.Map[String, Any]](new FileInputStream(file)).map(constructor().setMap(_))
-  }
+  def iterator = FrontletIO.fromJSONFile(new FileInputStream(file),constructor)
 
   def +=(f:F) {
     out.println(f.toJSON)
@@ -31,6 +25,18 @@ class FrontletJSONFile[F<:AbstractFrontlet](filename:String, constructor: () => 
 
   def flush() {
     out.flush()
+  }
+
+}
+
+/**
+ * IO Helpers for frontlets.
+ */
+object FrontletIO {
+  def fromJSONFile[F<:AbstractFrontlet](in:InputStream, constructor:() => F):Iterator[F#FrontletType] = {
+    val parser = FrontletJacksonMapper.mapper.getJsonFactory.createJsonParser(in)
+    val maps = Util.untilException(FrontletJacksonMapper.readValue[mutable.Map[String, Any]](parser), t => true)
+    maps.map(constructor().setMap(_))
   }
 
 }
